b9dd97c8f4f704f9395a4c0ae78c97a62c9d1b9b
Bohdan <bohdan.vasylyshyn@botscrew.com>
2016-11-18 10:31:55 +0200

    Initial commit

---
+++ src/main/java/com/botscrew/iaquest/IaquestApplication.java
@@ -0,0 +1,12 @@
+package com.botscrew.iaquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaquestApplication.class, args);
+	}
+}
